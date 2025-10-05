9e1974d8ffcc96e360e3ff9f0afc13fb74e92a89
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-10-06 00:05:24 +0530

    python script(Service check)

--- servicecheck.py
+++ servicecheck.py
@@ -1,8 +1,23 @@
+# import os
+
+# service = "nginx"
+
+# status = os.system(f"systemctl is-active --quiet {service}")
+
+# if (status == 0):
+#     print(f"The {service} service is active")
+# else:
+#     print(f"The {service} is not active")
+
+#Since systemctl is not recognised in windows, we can't check nginx service is running ot not. So for the demonstartion ,i'm using windows based service below.
+
+
+
 import os
 
-service = "nginx"
+service = "winDefend" #This is a window's service
 
-status = os.system(f"systemctl is-active --quiet {service}")
+status = os.system(f'sc query "{service}" | find "RUNNING"')
 
 if (status == 0):
     print(f"The {service} service is active")
