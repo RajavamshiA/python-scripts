5c0e08880f35d415ca1a7cd6ebc0fc45322e2ac2
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-10-05 23:34:52 +0530

    python script(Error count)

---
+++ errorcount.py
@@ -0,0 +1,12 @@
+#This scripts counts the no f error codes and print.
+def findErrorMsg(logfile):
+    count = 0
+    with open(logfile,'r') as f:
+        for line in f:
+            if "ERROR" in line:
+                print(line.strip())
+                count+=1
+    print(f"Number of ERROR codes in the log file are {count}")
+    
+
+findErrorMsg("error_log.txt")
